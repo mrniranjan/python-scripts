b8a90161db191d35dd1782156c95a879c4f7e7a8
Niranjan MR <mrniranjan@redhat.com>
2014-02-07 05:20:47 +0530

    Example script showing basic mathematics

---
+++ myex1.py
@@ -0,0 +1,4 @@
+print "Is 382 greater than 4872: ", 382 > 4872 
+print "3 + 2 - (5 * 3) + 15 - 2 + (3/5 * 4/5)"
+print "What would be the answer for below mathematical expression:",  3 + 2 - (5 * 3) + 15 - 2 + (3/5 * 4/5)
+
