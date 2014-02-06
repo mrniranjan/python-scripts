1c5ac135065a0f5d870e92232f083255879b8574
Niranjan MR <mrniranjan@redhat.com>
2014-02-07 05:08:20 +0530

    Basic Mathematics

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# Prints string "i will now count my chickens:" on console
+print "I will now count my chickens:"
+
+# prints string "Hens" and also calculates 25 + 30 / 6 and gives result
+print "Hens", 25 + 30 / 6
+# Prints string "Roosters" and also calculates 100 - 25 * 3 % 4, 
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints string "Now i will count the eggs"
+print "Now i will count the egss:"
+
+# Prints the result of the mathematical expression  3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6, it uses PODMAS method 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints string "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints True/False depending on the outcome of the expression (3 + 2 < 5 - 7)
+print 3 + 2 < 5 - 7
+
+# Prints string "what is 3 +2?" and then result of the expression 3 +2 is printed
+print "What is 3 + 2?", 3 + 2
+# Print string "What is 5 - 7?" and then then the result of the expression 5 - 7 is printed
+print "What is 5 - 7?", 5 - 7
+
+# Print string "Oh, that's why it's False"
+print "Oh, that's why it's False."
+
+# Print string "How about some more"
+print "How about some more."
+
+# Print "is it greater", and then prints true or false if 5 is greater than 2 
+print "Is it greater?", 5 > -2
+# Print "is it greater or equal?" and prints true if 5 >= -2 else prints false
+print "Is it greater or equal?", 5 >= -2 
+# Prints "Is it less or equal?" and then prints true if 5 <= -2 else prints false
+print "Is it less or equal?", 5 <= -2
