85ea6c65e129249090f00be910deb9771622d10d
Niranjan MR <mrniranjan@redhat.com>
2014-02-08 19:30:24 +0530

    Simple interest program without any variables

---
+++ myex3.py
@@ -0,0 +1,12 @@
+#This program calculates simple interest for principal amount of Rs 4000, Rate of interest is 5%
+# Number of Years is 8.5
+#Formula : si = p * n * r / 100 
+
+#This program doesn't use any Variables 
+
+print "%s has borrowed %d amount from the bank" % ('Ramesh', 4000)
+print "Bank calculates %d years at  %0.1f interest" % (5, 8.5)
+print "Simple intrest for %s would be: %0.1f" % ('Ramesh', 4000 * 8.5 * 5 / 100)
+
+
+
