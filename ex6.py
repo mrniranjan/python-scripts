26e3907a050b75ae81316532b3336e825d36f53e
Niranjan MR <mrniranjan@redhat.com>
2014-02-08 07:36:36 +0530

    Ex-6 More printing variables

---
+++ ex6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+#print string "Mary had a little lamb."
+print "Mary had a little lamb."
+#print string "It's fleece was white as %s.", %s will be replaced with 'snow'
+print "It's fleece was white as %s." % 'snow'
+#print string "And everyhwere that Mary went"
+print "And everywhere that Mary went."
+#print "." 10 times
+print "." * 10 
+#declare variables end1 to end12, with each characters from Cheese Burger"
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#print variables from end1 to end6 , and then print values in variables end7 to end12 without any line break
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
