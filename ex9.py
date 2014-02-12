db9efdc6b99194841e578a60043587d736fb9042
Niranjan MR <mrniranjan@redhat.com>
2014-02-12 10:25:03 +0530

    Added new example files

---
+++ ex9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+days = "Mon Tue Wed Thu Fri Sat Sun"
+Months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print "Here are the days:", days
+print "Here are the months:", Months
+
+print """
+There's something going on here
+with the three double quotes
+We will be able to type as much as we like. 
+Even 4 lines if we want or 5, or 6
+"""
+
